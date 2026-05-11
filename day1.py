d57351c3837de206ae3e9c4e094c7e5a1e618c5e
SOLO WOLFESS <itsyashiiiiii2711@gmail.com>
2026-05-12 00:36:16 +0500

    first upload of 30 days of python

---
+++ day1.py
@@ -0,0 +1,25 @@
+print (3+4) # performing different operations using operand 3 and 4
+print (3-4)
+print (3*4)
+print (3/4)
+print (3**4)
+print (3//4)
+print (3%4)
+print ("My name is Yashfa") #single line comment
+print ("My family name is Naveed Ch") # single line comment
+print ("I am Pakistani") #signle line comment
+print ("""
+I am not enjoying my 30 days of python too well bcoz my chachoo is a little bit strict but he is indeed a good tutor as well:)
+""")
+print(type("ABC"))
+print(type("10")) #any text in quotes is considered as string hwether its alphabet or number 
+print(type(9.9))
+print(type(10))
+print(type(True))
+print(type(False))
+print(type(3+4j))
+print(type("Yashfa; Pakistan; Python"))
+print(type(('Yashfa','Pakistan','Python'))) #in tuple rounded brackets () are used, tuples are unchangeable and ordered
+print(type(['Yashfa','Pakistan','Python'])) #in list square brackets [] are used, lists are changeable and ordered
+print(type({'Yashfa','Pakistan','Python'})) #in set curly or curved brackets {} are used, sets have unique and unduplicated value
+print(type({"name":"Yashfa", "age": 21 })) #in dictionary curved brackets {} are used along with colon : to represent key and its value
