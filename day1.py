b8c820efd58f6b838821d3c0a92bd9391f776175
SOLO WOLFESS <itsyashiiiiii2711@gmail.com>
2026-05-12 21:31:47 +0500

    updated day1 file

--- day1.py
+++ day1.py
@@ -7,12 +7,12 @@ print (3//4)
 print (3%4)
 print ("My name is Yashfa") #single line comment
 print ("My family name is Naveed Ch") # single line comment
-print ("I am Pakistani") #signle line comment
+print ("I am Pakistani") #single line comment
 print ("""
 I am not enjoying my 30 days of python too well bcoz my chachoo is a little bit strict but he is indeed a good tutor as well:)
-""")
+""") # multi-line comment
 print(type("ABC"))
-print(type("10")) #any text in quotes is considered as string hwether its alphabet or number 
+print(type("10")) #any text in quotes is considered as string whether its alphabet or number or any text
 print(type(9.9))
 print(type(10))
 print(type(True))
@@ -23,3 +23,11 @@ print(type(('Yashfa','Pakistan','Python'))) #in tuple rounded brackets () are us
 print(type(['Yashfa','Pakistan','Python'])) #in list square brackets [] are used, lists are changeable and ordered
 print(type({'Yashfa','Pakistan','Python'})) #in set curly or curved brackets {} are used, sets have unique and unduplicated value
 print(type({"name":"Yashfa", "age": 21 })) #in dictionary curved brackets {} are used along with colon : to represent key and its value
+#eucledean distance
+import math
+x1 = 5
+y1 = 4
+x2 = 6
+y2 = 7
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(distance)
